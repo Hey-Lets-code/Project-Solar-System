289226841d65e8d0de3c07040282d925740e560f
leticia <le.augusta.souza@gmail.com>
2022-08-18 18:47:11 -0300

    Requisito 3 - 18 de ago 2022 às 18:47

---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Title extends React.Component {
+  render() {
+    const { headline } = this.props;
+    return (
+      <h2>{ headline }</h2>
+    );
+  }
+}
+Title.propTypes = { // headline é o parâmetro, headline é obrigatório e tem que ser do tipo string
+  headline: PropTypes.string.isRequired, // string: o PropTypes define o tipo de dado
+};
+export default Title;
