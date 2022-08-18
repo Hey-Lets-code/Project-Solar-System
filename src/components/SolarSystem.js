e631416fa1cdab59b9423366b8f2aa67d6dc088b
leticia <le.augusta.souza@gmail.com>
2022-08-18 19:08:22 -0300

    Requisito 4 - 18 de ago 2022 às 19:08

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Title from './Title';
 
 class SolarSystem extends React.Component { // ela é uma cópia do componente react
   render() {
     return (
-      <div data-testid="solar-system" />);
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+      </div>);
   }
 }
 
